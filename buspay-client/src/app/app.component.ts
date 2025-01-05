888e862e53f5f2836844fc07897493cebaf96505
Im-your-hari <baluoger91@gmail.com>
2025-01-06 00:15:32 +0530

    Analytics card basic structure added

--- buspay-client/src/app/app.component.ts
+++ buspay-client/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideBarComponent } from './private/common-components/side-bar/side-bar.component';
+import { AnalyticsCardComponent } from './private/common-components/analytics-card/analytics-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,SideBarComponent],
+  imports: [RouterOutlet,SideBarComponent,AnalyticsCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ buspay-client/src/app/private/common-components/analytics-card/analytics-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-analytics-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './analytics-card.component.html',
+  styleUrl: './analytics-card.component.scss'
+})
+export class AnalyticsCardComponent {
+
+}
