eacf0b403b34b9ec0913fd0f3252f212e75aa34c
Im-your-hari <baluoger91@gmail.com>
2025-01-05 19:05:13 +0530

    Ag-grid added without common issues

--- buspay-client/src/app/app.component.ts
+++ buspay-client/src/app/app.component.ts
@@ -1,13 +1,52 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DataGridComponent } from './private/common-components/data-grid/data-grid.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,DataGridComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'buspay-client';
+
+  // Row Data: The data to be displayed.
+  rowData = [
+      { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+      { make: "Ford", model: "F-Series", price: 33850, electric: false },
+      { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+      { make: "Mercedes", model: "EQA", price: 48890, electric: true },
+      { make: "Fiat", model: "500", price: 15774, electric: false },
+      { make: "Nissan", model: "Juke", price: 20675, electric: false },
+      { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+      { make: "Ford", model: "F-Series", price: 33850, electric: false },
+      { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+      { make: "Mercedes", model: "EQA", price: 48890, electric: true },
+      { make: "Fiat", model: "500", price: 15774, electric: false },
+      { make: "Nissan", model: "Juke", price: 20675, electric: false },
+      { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+      { make: "Ford", model: "F-Series", price: 33850, electric: false },
+      { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+      { make: "Mercedes", model: "EQA", price: 48890, electric: true },
+      { make: "Fiat", model: "500", price: 15774, electric: false },
+      { make: "Nissan", model: "Juke", price: 20675, electric: false },
+      { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+      { make: "Ford", model: "F-Series", price: 33850, electric: false },
+      { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+      { make: "Mercedes", model: "EQA", price: 48890, electric: true },
+      { make: "Fiat", model: "500", price: 15774, electric: false },
+      { make: "Nissan", model: "Juke", price: 20675, electric: false },
+    ];
+
+    // Column Definitions: Defines & controls grid columns.
+  colDefs: any[] = [
+    { field: "make" ,headerName: "MAKE",filter:true},
+    { field: "model" ,filter:true},
+    { field: "price",cellRenderer:(item: any)=>{ 
+      return `$${item.value}`
+    },filter:true ,editable:true},
+    { field: "electric" },
+  ];
 }
---
+++ buspay-client/src/app/private/common-components/data-grid/data-grid.component.ts
@@ -0,0 +1,42 @@
+import { Component, Input } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular'; 
+import type { ColDef, GridApi, GridReadyEvent } from 'ag-grid-community';
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community'; 
+
+// Register all Community features
+ModuleRegistry.registerModules([AllCommunityModule]);
+
+@Component({
+  selector: 'app-data-grid',
+  standalone: true,
+  imports: [AgGridAngular],
+  templateUrl: './data-grid.component.html',
+  styleUrl: './data-grid.component.scss'
+})
+export class DataGridComponent {
+  @Input() rowData:any
+  @Input() colDefs!:ColDef[]
+  @Input() isExported:boolean = false
+  @Input() exportLabel:string="EXPORT"
+  @Input() pagination:boolean=false
+  @Input() paginationPageSizeSelector:any[]=[5,10,100]
+
+  private gridApi!:GridApi<any>
+
+  constructor(){}
+
+
+  defaultColDef: ColDef = {
+    flex: 1,
+    minWidth:100
+  };
+
+  onGridReady(event:GridReadyEvent<any>):void {
+    this.gridApi = event.api
+  }
+
+  onExportClick(){
+    this.gridApi.exportDataAsCsv()
+  }
+
+}
