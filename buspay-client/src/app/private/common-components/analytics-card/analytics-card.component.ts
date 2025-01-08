3d253b2072c059bd7472c201f747570343512d56
Im-your-hari <baluoger91@gmail.com>
2025-01-09 00:11:18 +0530

    Analytics card added

--- buspay-client/src/app/private/common-components/analytics-card/analytics-card.component.ts
+++ buspay-client/src/app/private/common-components/analytics-card/analytics-card.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-analytics-card',
   standalone: true,
   imports: [],
   templateUrl: './analytics-card.component.html',
-  styleUrl: './analytics-card.component.scss'
+  styleUrl: './analytics-card.component.scss',
 })
 export class AnalyticsCardComponent {
-
+  @Input() title: string = '';
+  @Input() titleIcon!: any;
+  @Input() analyticValue: string = '';
+  @Input() trendLineIcon!: any;
+  @Input() trendLine: string = '';
+  @Input() trendLineImage!: any;
 }
