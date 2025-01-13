2350cedf81be8136ecf0bd24e9862253d3a4f177
Im-your-hari <baluoger91@gmail.com>
2025-01-13 23:02:29 +0530

    common modal basic added

--- buspay-client/src/app/app.component.ts
+++ buspay-client/src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModalComponent } from './private/common-components/common-modal/common-modal.component';
+import { CommonModalService } from './private/common-components/common-modal/common-modal.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModalComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'buspay-client';
+  @ViewChild('sample') sample!: TemplateRef<any>;
+
+  constructor(private modalService: CommonModalService) {}
+
+  onClick() {
+    this.modalService.showModal({
+      heading: 'Dynamic Modal',
+      content: this.sample,
+      isHeaderRequired: true,
+      isFooterRequired: true,
+    });
+  }
 }
