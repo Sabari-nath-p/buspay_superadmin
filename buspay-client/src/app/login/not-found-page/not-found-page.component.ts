6c871fa35cd4443028fc1e7b5b5ebe0a998e7247
Im-your-hari <baluoger91@gmail.com>
2025-03-02 02:56:48 +0530

    not found page

---
+++ buspay-client/src/app/login/not-found-page/not-found-page.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-not-found-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './not-found-page.component.html',
+  styleUrl: './not-found-page.component.scss',
+})
+export class NotFoundPageComponent {
+  constructor(private location: Location) {}
+
+  goBack(): void {
+    this.location.back();
+  }
+}
